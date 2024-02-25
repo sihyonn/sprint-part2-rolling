3d12554b4fb428a6f3086d9fce0bb0753aedfea7
sihyonn <sihyeon982@naver.com>
2024-02-26 02:28:52 +0900

    init: 프로젝트 생성 및 초기 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>롤링프로젝트 초기세팅중...</>;
+}
+
+export default App;
