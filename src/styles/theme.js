8234447fe330f0e4e6006486c5da6e750b141f35
sihyonn <sihyeon982@naver.com>
2024-02-26 04:53:00 +0900

    style: globalStyle, theme 초기세팅

---
+++ src/styles/theme.js
@@ -0,0 +1,60 @@
+const theme = Object.freeze({
+  color: {
+    // Purple
+    mainPu: '#9935FF',
+    hoverPu: '#861DEE',
+    pressePu: '#6E0AD1',
+    focusPu: '#6E0AD1',
+    lightPu1: '#F8F0FF',
+    lightPu2: '#ECD9FF',
+
+    // Orange
+    textOr: '#FF8832',
+    lightOr1: '#FFF0D6',
+    lightOr2: '#FFE2AD',
+
+    // Blue
+    textBl: '#00A2FE',
+    lightBl1: '#E2F5FF',
+    lightBl2: '#B1E4FF',
+
+    // Green
+    textGn: '#2BA600',
+    lightGn1: '#E4FBDC',
+    lightGn2: '#D0F5C3',
+
+    // Gray
+    lightGr: '#F6F6F6',
+    mainGr: '#CCCCCC',
+    btnGr: '#555555',
+
+    white: '#FFFFFF',
+    black: '#000000',
+    red: '#DC3A3A',
+    surface: '#F6F8FF',
+    badgeBg: '#0000008A',
+  },
+
+  boder: {
+    // Gray
+    gr1: '1px solid #CCCCCC',
+    gr2: '1px solid #555555',
+
+    //Purple
+    pu1: '1px solid #9935FF',
+    pu2: '1px solid #6E0AD1',
+    pu3: '2px solid #9935FF',
+  },
+
+  boxShadow: {
+    card: '0px 2px 12px 0px rgba(0, 0, 0, 0.08)',
+  },
+
+  fontFamily: {
+    main: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
+    logo: "'Teko', sans-serif",
+  },
+  //글씨 사이즈 weight 이어서
+});
+
+export default theme;
