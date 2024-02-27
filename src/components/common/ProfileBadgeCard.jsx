b419ded564a290cd7cfd1b4401619b23cf86355c
김현일 <hyunonetwo@gmail.com>
2024-02-28 01:52:07 +0900

    Feat:프로필 공통 컴포넌트 구현

---
+++ src/components/common/ProfileBadgeCard.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import { css } from 'styled-components';
+
+const setBackgroundColor = css`
+  background-color: ${(props) => (props.profileImg ? 'transparent' : '#fff')};
+`;
+
+const Styled = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 5.6rem; /* 70% of 8rem */
+    height: 5.6rem; /* 70% of 8rem */
+    padding: 1.68rem;
+
+    border-radius: 100px;
+    border: 1px solid #eee;
+    background-color: ${({ theme }) => theme.color.mainGr};
+    background-image: url(${(props) => props.profileImg || ''});
+    background-position: top;
+    background-size: cover;
+  `,
+  Div: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.245rem; /* 70% of 0.35rem */
+  `,
+  Head: styled.div`
+    width: 1.05rem; /* 70% of 1.5rem */
+    height: 1.05rem; /* 70% of 1.5rem */
+    border-radius: 10rem;
+    ${setBackgroundColor}
+  `,
+  Body: styled.div`
+    width: 2.11rem;
+    height: 0.91rem;
+    border-radius: 15.707px 15.707px 3.141px 3.141px;
+    ${setBackgroundColor}
+  `,
+};
+
+function ProfileBadgeCard({ profileImg }) {
+  return (
+    <Styled.Container profileImg={profileImg}>
+      <Styled.Div>
+        <Styled.Head profileImg={profileImg} />
+        <Styled.Body profileImg={profileImg} />
+      </Styled.Div>
+    </Styled.Container>
+  );
+}
+
+export default ProfileBadgeCard;
--- src/pages/Hi.jsx
+++ src/pages/Hi.jsx
@@ -1,7 +1,21 @@
+import ProfileBadgeCard from 'src/components/common/ProfileBadgeCard';
+import ProfileBadgeSelect from 'src/components/common/ProfileBadgeSelect';
 import React from 'react';
 
+const mockData = {
+  default: '',
+  img1: 'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+};
+
 function Hi() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <ProfileBadgeCard profileImg={mockData.img1} />
+      <ProfileBadgeCard profileImg={mockData.default} />
+      <ProfileBadgeSelect profileImg={mockData.img1} />
+      <ProfileBadgeSelect profileImg={mockData.default} />
+    </>
+  );
 }
 
 export default Hi;
