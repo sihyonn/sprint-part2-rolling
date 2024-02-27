8ae2fa2f046edbf82358d233aeb6660d80bac7b3
김현일 <hyunonetwo@gmail.com>
2024-02-27 17:56:28 +0900

    Refactor:theme 활용을 위한 리팩토링

--- src/pages/Hi.jsx
+++ src/pages/Hi.jsx
@@ -1,7 +1,15 @@
+import RelationBadge from '@components/common/RelationBadge';
 import React from 'react';
 
 function Hi() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <RelationBadge type={'동료'}></RelationBadge>
+      <RelationBadge type={'지인'}></RelationBadge>
+      <RelationBadge type={'가족'}></RelationBadge>
+      <RelationBadge type={'친구'}></RelationBadge>
+    </>
+  );
 }
 
 export default Hi;
