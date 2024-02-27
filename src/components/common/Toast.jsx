b7d48b4446615e8202d4429e27d1a09efcce4d49
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-02-27 21:39:09 +0900

    Fix: Toast.jsx 수정

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -7,27 +7,25 @@ import close from '@/assets/closeIcon.svg';
 const Styled = {
   ToastContainer: styled.div`
     display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
     position: fixed;
     bottom: 5rem;
-    width: 52.4rem;
-    height: 6.4rem;
+    min-width: 52.4rem;
     z-index: 2;
     padding: 1.9rem 3rem;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 25.3rem;
     border-radius: 0.8rem;
     background: rgba(0, 0, 0, 0.8);
+
     @media (max-width: 560px) {
-      width: 32rem;
-      height: 6.4rem;
-      gap: 4.9rem;
+      min-width: 32rem;
     }
   `,
   MessageContainer: styled.div`
     display: flex;
     align-items: center;
     gap: 1.2rem;
+
     span {
       color: ${({ theme }) => theme.color.white};
       font-family: Pretendard;
@@ -39,15 +37,14 @@ const Styled = {
     }
   `,
   CloseButton: styled.button`
-    cursor: 'pointer';
-    background: 'none';
-    border: 'none';
+    cursor: pointer;
+    background: none;
+    border: none;
   `,
 };
 function CustomToast({ id, message }) {
   const handleClose = () => {
     toast.remove(id);
-    console.log(id);
   };
   return (
     <Styled.ToastContainer>
