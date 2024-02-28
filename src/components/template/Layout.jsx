fd48c21ee042b7d3f9f4c3255a333c6f1758e0a5
sihyonn <sihyeon982@naver.com>
2024-02-28 23:58:41 +0900

    feat: 전체페이지 레이아웃 적용

--- src/components/template/Layout.jsx
+++ src/components/template/Layout.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
+import Gnb from '@components/common/Gnb';
+
+const Styled = {
+  Container: styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.color.white};
+    padding-top: 6.6rem;
+  `,
+};
 
 function Layout({ children }) {
   return (
-    <div>
-      gnb 들어갈 예정!
-      <Outlet />
-      {children}
-    </div>
+    <>
+      <Gnb />
+      <Styled.Container>
+        <Outlet />
+        {children}
+      </Styled.Container>
+    </>
   );
 }
 
