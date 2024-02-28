e0b301173c8c758f3a864bbed1ab277dd8380e9a
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-02-29 06:47:36 +0900

    Fix: 버튼 오류 수정

--- src/components/common/Gnb.jsx
+++ src/components/common/Gnb.jsx
@@ -53,9 +53,6 @@ const Styled = {
     line-height: 2.6rem;
     letter-spacing: -0.02rem;
     text-decoration: none;
-    @media (min-width: 768px) and (max-width: 1280px) {
-      display: none;
-    }
   `,
 };
 
