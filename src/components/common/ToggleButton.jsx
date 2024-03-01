7c6ac0a7151927da29cf0ce803ad0ac3559a0797
ggjiny <jnss599@gmail.com>
2024-03-01 20:51:25 +0900

    Refactor: ToggleButton isToggled state 객체로 수정

--- src/components/common/ToggleButton.jsx
+++ src/components/common/ToggleButton.jsx
@@ -35,23 +35,30 @@ const Styled = {
 };
 
 const ToggleButton = ({ className, onToggle }) => {
-  const [isToggled, setIsToggled] = useState(true);
+  const initialToggle = {
+    컬러: false,
+    이미지: false,
+  };
+  const [isToggled, setIsToggled] = useState({ ...initialToggle, 컬러: true });
 
   const handleToggle = (value) => {
-    setIsToggled(!isToggled);
+    setIsToggled(() => ({
+      ...initialToggle,
+      [value]: true,
+    }));
     onToggle(value);
   };
 
   return (
     <Styled.ToggleContainer className={className}>
       <Styled.Toggle
-        $isToggled={isToggled}
+        $isToggled={isToggled['컬러']}
         onClick={() => handleToggle('컬러')}
       >
         컬러
       </Styled.Toggle>
       <Styled.Toggle
-        $isToggled={!isToggled}
+        $isToggled={isToggled['이미지']}
         onClick={() => handleToggle('이미지')}
       >
         이미지
