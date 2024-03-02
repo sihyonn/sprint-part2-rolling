322063b4c9bec0a72964cd48ab817dcc7e23beb9
sihyonn <sihyeon982@naver.com>
2024-03-03 00:36:31 +0900

    feat: QueryClient 생성,QueryClientProvider 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Toaster } from 'react-hot-toast';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
-import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
+
 import router from '@/router';
 import theme from '@styles/theme.js';
 import GlobalStyle from '@styles/GlobalStyle.js';
 
 function App() {
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error, query) => {
+        console.log(error, query);
+      },
+    }),
+  });
+
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Toaster />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Toaster />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
   );
 }
 
