f226d9871c3dd0510f78ea637d90f6dceabf1fab
sihyonn <sihyeon982@naver.com>
2024-03-02 17:24:57 +0900

    feat: Toast 컴포넌트에 showErrorToast 추가

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -3,6 +3,7 @@ import { toast } from 'react-hot-toast';
 import styled from 'styled-components';
 import completed from '@/assets/completedIcon.svg';
 import close from '@/assets/closeIcon.svg';
+import errorIcon from '@/assets/icon/ErrorIcon.png';
 
 const Styled = {
   ToastContainer: styled.div`
@@ -16,11 +17,26 @@ const Styled = {
     padding: 1.9rem 3rem;
     border-radius: 0.8rem;
     background: rgba(0, 0, 0, 0.8);
+    animation: fadeInOut 5s;
 
     @media (max-width: 560px) {
       min-width: 32rem;
     }
+
+    @keyframes fadeInOut {
+      0%,
+      100% {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      10%,
+      90% {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
   `,
+
   MessageContainer: styled.div`
     display: flex;
     align-items: center;
@@ -36,20 +52,24 @@ const Styled = {
       letter-spacing: -0.016rem;
     }
   `,
+
   CloseButton: styled.button`
     cursor: pointer;
     background: none;
     border: none;
   `,
 };
-function CustomToast({ id, message }) {
+function CustomToast({ id, message, type }) {
   const handleClose = () => {
     toast.remove(id);
   };
+
+  const icon = type === 'error' ? errorIcon : completed;
+
   return (
     <Styled.ToastContainer>
       <Styled.MessageContainer>
-        <img src={completed} alt="completedIcon" />
+        <img src={icon} alt={`${type}Icon`} style={{ width: '2.4rem' }} />
         <span>{message}</span>
       </Styled.MessageContainer>
       <Styled.CloseButton type="button" onClick={handleClose}>
@@ -59,9 +79,13 @@ function CustomToast({ id, message }) {
   );
 }
 
-export const showToast = (message) => {
-  toast.custom((t) => <CustomToast id={t.id} message={message} />, {
+export const showToast = (message, type = 'completed') => {
+  toast.custom((t) => <CustomToast id={t.id} message={message} type={type} />, {
     duration: 5000,
     position: 'bottom-center',
   });
+};
+
+export const showErrorToast = (message) => {
+  showToast(message, 'error');
 };
