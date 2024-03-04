8ebd206276addbc4fa86d402b3152c1c833774fe
sihyonn <sihyeon982@naver.com>
2024-03-05 01:54:12 +0900

    feat: InfiniteCardMessages 그리드템플릿 적용, 페이지 무한카드메세지 적용

--- src/components/rollingPaperViewer/InfiniteCardMessages.jsx
+++ src/components/rollingPaperViewer/InfiniteCardMessages.jsx
@@ -1,8 +1,11 @@
-import useInfiniteCardMessagesQuery from '@hooks/api/messagesAPI/useInfiniteCardMessagesQuery';
 import React from 'react';
-import Card from './Card';
-import useIntersectionObserver from '@hooks/useIntersectionObserver';
 import { useRef } from 'react';
+
+import Card from '@/components/rollingPaperViewer/Card';
+import AddCard from '@/components/rollingPaperViewer/AddCard';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
+import useInfiniteCardMessagesQuery from '@hooks/api/messagesAPI/useInfiniteCardMessagesQuery';
+import { GridTemplate } from '@styles/commonStyle';
 
 function InfiniteCardMessages() {
   const recipientId = 4028;
@@ -11,20 +14,31 @@ function InfiniteCardMessages() {
   const { data: cardMessagesData, fetchNextPage } =
     useInfiniteCardMessagesQuery(recipientId);
 
-  console.log('데이터', cardMessagesData, fetchNextPage);
+  const isLastPage = cardMessagesData?.pages?.at(-1)?.next === null;
+  console.log(isLastPage);
 
   useIntersectionObserver(async () => {
     await fetchNextPage();
   }, loaderRef);
 
+  console.log(cardMessagesData);
+
   return (
     <>
-      {cardMessagesData?.pages.map((page) =>
-        page.results.map((result) => <Card key={result.id} data={result} />),
-      )}
+      <GridTemplate>
+        <AddCard recipientId={recipientId} />
+        {cardMessagesData?.pages.map((page) =>
+          page.results.map((result) => <Card key={result.id} data={result} />),
+        )}
+      </GridTemplate>
+      {/* todo 아래 스켈레톤 로더로 대체하기 */}
       <div
         ref={loaderRef}
-        style={{ height: '20px', margin: '10px', backgroundColor: 'lightgray' }}
+        style={
+          isLastPage
+            ? { display: 'none' }
+            : { height: '20px', margin: '10px', backgroundColor: 'lightgray' }
+        }
         aria-label="Loading more messages"
       >
         로딩중이라네
