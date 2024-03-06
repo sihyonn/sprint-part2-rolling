4fa0222cc6dc48f645ca708bb5620bff9607d33e
sihyonn <sihyeon982@naver.com>
2024-03-07 02:43:19 +0900

    feat: 로더 적용

--- src/pages/PaperViewerPage.jsx
+++ src/pages/PaperViewerPage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
 
-import InfiniteCardMessages from '@components/rollingPaperViewer/InfiniteCardMessages';
 import Button from '@components/common/button/Button';
 import routes from '@constants/routes';
+import InfiniteCardMessages from '@components/rollingPaperViewer/InfiniteCardMessages';
+import InfiniteCardMessagesLoader from '@components/rollingPaperViewer/InfiniteCardMessagesLoader';
 
 const Styled = {
   ButtonContainer: styled.div`
@@ -47,7 +48,9 @@ function PaperViewerPage() {
         </Button>
       </Styled.ButtonContainer>
 
-      <InfiniteCardMessages recipientId={recipientId} />
+      <React.Suspense fallback={<InfiniteCardMessagesLoader />}>
+        <InfiniteCardMessages recipientId={recipientId} />
+      </React.Suspense>
     </>
   );
 }
