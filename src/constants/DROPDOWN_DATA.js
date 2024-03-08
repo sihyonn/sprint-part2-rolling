2141b58c115579aa7f3b25ef154ba21a255add4b
sihyonn <sihyeon982@naver.com>
2024-03-08 17:42:34 +0900

    feat: 상수 Object.freeze() 적용

--- src/constants/DROPDOWN_DATA.js
+++ src/constants/DROPDOWN_DATA.js
@@ -1,7 +1,7 @@
-export const DROPDOWN_DATA = {
+export const DROPDOWN_DATA = Object.freeze({
   FONT: {
     OPTIONS: ['Noto Sans', 'Pretendard', '나눔명조', '나눔손글씨 손편지체'],
     DEFAULT: 'Noto Sans',
   },
   RELATIONSHIP: { OPTIONS: ['친구', '지인', '동료', '가족'], DEFAULT: '지인' },
-};
+});
