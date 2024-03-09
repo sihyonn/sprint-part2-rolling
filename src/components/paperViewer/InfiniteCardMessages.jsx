0ae74eb9ba0a912bfc412570386f1a93a23b14be
sihyonn <sihyeon982@naver.com>
2024-03-09 21:45:04 +0900

    feat: 편집페이지 메시지 없을 경우 알림 문구 적용

--- src/components/paperViewer/InfiniteCardMessages.jsx
+++ src/components/paperViewer/InfiniteCardMessages.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { useRef } from 'react';
+import { styled } from 'styled-components';
 
 import Card from '@components/paperViewer/Card';
 import AddCard from '@components/paperViewer/AddCard';
+import InfiniteCardMessagesLoader from '@/components/paperViewer/InfiniteCardMessagesLoader';
 import useIntersectionObserver from '@hooks/useIntersectionObserver';
 import useInfiniteCardMessagesQuery from '@hooks/api/messagesAPI/useInfiniteCardMessagesQuery';
-import InfiniteCardMessagesLoader from './InfiniteCardMessagesLoader';
 import { GridTemplate } from '@styles/commonStyle';
+
+const Styled = {
+  EmptyMessage: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    top: 9rem;
+
+    font-size: 2rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.white};
+
+    div {
+      font-size: 9rem;
+      margin-bottom: 2rem;
+    }
+  `,
+};
 
 /**
  * InfiniteCardMessages - 카드 메시지 무한 스크롤
@@ -20,6 +40,7 @@ function InfiniteCardMessages({ recipientId, isEditPage }) {
     useInfiniteCardMessagesQuery(recipientId, isEditPage);
 
   const isLastPage = cardMessagesData?.pages?.at(-1)?.next === null;
+  const hasData = !(cardMessagesData?.pages?.at(-1).count === 0);
 
   useIntersectionObserver(async () => {
     await fetchNextPage();
@@ -27,20 +48,29 @@ function InfiniteCardMessages({ recipientId, isEditPage }) {
 
   return (
     <>
-      <GridTemplate>
-        {!isEditPage && <AddCard recipientId={recipientId} />}
-        {cardMessagesData?.pages.map((page) =>
-          page.results.map((result) => (
-            <Card key={result.id} data={result} isEditPage={isEditPage} />
-          )),
-        )}
-      </GridTemplate>
+      {isEditPage && !hasData ? (
+        <Styled.EmptyMessage>
+          <div>📨</div>
+          편집할 카드 메시지가 없어요!
+        </Styled.EmptyMessage>
+      ) : (
+        <>
+          <GridTemplate>
+            {!isEditPage && <AddCard recipientId={recipientId} />}
+            {cardMessagesData?.pages.map((page) =>
+              page.results.map((result) => (
+                <Card key={result.id} data={result} isEditPage={isEditPage} />
+              )),
+            )}
+          </GridTemplate>
 
-      <InfiniteCardMessagesLoader
-        className="skeleton"
-        loaderRef={loaderRef}
-        style={isLastPage ? { display: 'none' } : { marginTop: '2rem' }}
-      />
+          <InfiniteCardMessagesLoader
+            className="skeleton"
+            loaderRef={loaderRef}
+            style={isLastPage ? { display: 'none' } : { marginTop: '2rem' }}
+          />
+        </>
+      )}
     </>
   );
 }
