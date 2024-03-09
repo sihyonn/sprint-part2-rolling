cec94a4d2056aec4f78c43b4fc22c8ef30764729
김현일 <hyunonetwo@gmail.com>
2024-03-09 16:34:17 +0900

    Refactor: 태블릿 및 모바일 화면에서 드래그할 때, 왼쪽 여백 삭제

--- src/components/ListPage/Carousel.jsx
+++ src/components/ListPage/Carousel.jsx
@@ -15,8 +15,14 @@ const Styled = {
     .slick-list {
       width: 1198px;
       transform: translate3d(0, 0, 0);
+      @media (min-width: 768px) and(max-width: 1247px) {
+        margin-left: -24px;
+        padding-left: 16px;
+      }
       @media (max-width: 767px) {
         width: 900px;
+        margin-left: -24px;
+        padding-left: 16px;
       }
     }
   `,
